03efc029bf339db25d44f23a4f2e90bc01895340
Nickater100 <nicosuasnavar@gmail.com>
2022-07-28 16:25:49 -0300

    first commit

--- js/Inicio.js
+++ js/Inicio.js
@@ -6,8 +6,15 @@ var inicio = {
   iniciarJuego: function() {
    console.log("juego iniciado");
    dimensiones.iniciar();
-   var r = new rectangulo(0, 0, 100, 100);
-   var r2 = new rectangulo(100, 0, 100, 100);
+   inicio.recargarTiles();
    buclePrincipal.iterar();
+  }, 
+  recargarTiles: function(){
+   document.getElementById("juego").innerHTML = "";
+   for (let y = 0; y < dimensiones.obtenerTilesVerticales(); y++) {
+    for (let x = 0; x < dimensiones.obtenerTilesHorizontales(); x++) {
+        var r = new rectangulo(x * dimensiones.ladoTiles, y * dimensiones.ladoTiles, dimensiones.ladoTiles, dimensiones.ladoTiles);
+    }
+   }
   }
 };
