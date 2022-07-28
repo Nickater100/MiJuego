247d94b9163502528fa2e933e6a9f30003a09d29
Nickater100 <nicosuasnavar@gmail.com>
2022-07-28 13:55:08 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset= "utf-8">
+    <tittle>Juego web</tittle>
+    <meta name="description" content="Esto es un juego web">
+    <meta name="author" content="Juego iniciado">
+<head>
+<body>
+    <h1>Bienvenido</h1>
+    <h3>Juego iniciado</h3>
+    <script src="js/inicio.js"></script>
+</body>
+</html>
