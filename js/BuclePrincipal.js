399958e648a51d25497de00ed7bf1623fea22948
Nickater100 <nicosuasnavar@gmail.com>
2022-07-28 14:36:25 -0300

    first commit

---
+++ js/BuclePrincipal.js
@@ -0,0 +1,29 @@
+let buclePrincipal = {
+    idEjecucion: null,
+    ultimoRegistro: 0,
+    APS: 0,      // Actualizaciones por segundo
+    FPS: 0,      // Frames por segundo
+    iterar: function(registroTemporal){
+      buclePrincipal.idEjecucion = window.requestAnimationFrame(buclePrincipal.iterar);
+
+      buclePrincipal.actualizar(registroTemporal);
+      buclePrincipal.dibujar();
+
+      if (registroTemporal - buclePrincipal.ultimoRegistro > 999) {
+        buclePrincipal.ultimoRegistro = registroTemporal;
+        console.log("APS: " + buclePrincipal.APS + " | FPS: " + buclePrincipal.FPS);
+        buclePrincipal.APS = 0;
+        buclePrincipal.FPS = 0;
+      }
+
+    },
+    detener: function(){
+
+    },
+    actualizar: function(registroTemporal){
+      buclePrincipal.APS++;
+    },
+    dibujar: function(registroTemporal){
+      buclePrincipal.FPS++;
+    }
+};
