53c73610850e70d477e42f6104806c2a4e842246
Nickater100 <nicosuasnavar@gmail.com>
2022-08-03 14:18:25 -0300

    inserta Mensaje aquí

--- js/Mando.js
+++ js/Mando.js
@@ -1,5 +1,39 @@
 var mando = {
     objeto: null,
     eventosDisponibles: "ongamepadconnected" in window,
-    
+    conectado: false,
+    iniciar: function(){
+     if (mando.eventosDisponibles) {
+        window.addEventListener("gamepadconnected", mando.conectar);
+        window.addEventListener("gamepaddisconnected", mando.desconectar)
+     } else {
+        mando.actualizar();
+     }
+    },
+    conectar:function(e){
+        mando.objeto = e.gamepad;
+        mando.identificar();
+    },
+    desconectar: function(e){
+        console.log("mando desconectado del indice %d: %s.", e.gamepad.index, e.gamepad.id)
+    },
+    actualizar: function(e){
+        if(!mando.eventosDisponibles){
+            mandos = null;
+            try {
+                mandos = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads: []);
+                mando.objeto = mandos[0];
+                if(!mando.conectado){
+                    mando.conectado = true;
+                    mando.identificar();
+                }
+            } catch (error) {
+                console.log(error.message);
+            }
+        }
+        if(mando.objeto){
+            return;
+        }
+    },
+    identificar: function(e){},
 }
